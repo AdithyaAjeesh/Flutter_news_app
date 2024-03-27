60efd318c0271a0000613afd2e4e2e5e691a9b79
Adithya Ajeesh Tk <adithyaajeesh911@gmail.com>
2024-03-27 12:44:24 +0530

    update

---
+++ lib/model/article_model.dart
@@ -0,0 +1,14 @@
+class Article {
+  Source? source;
+  String? author;
+  String? title;
+  String? discription;
+  String? url;
+  String? urlToImage;
+  String? publishedAt;
+  String? content;
+  
+}
+
+class Source {
+}
